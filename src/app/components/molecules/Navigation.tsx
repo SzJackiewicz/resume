0c30792323468aefc0afd820c2a85cc27e10ef24
Szymon Jackiewicz <sz.jackiewicz@gmail.com>
2024-09-16 19:59:03 +0200

    HEADER with nav styling

--- src/app/components/molecules/Navigation.tsx
+++ src/app/components/molecules/Navigation.tsx
@@ -1,3 +1,26 @@
 export const Navigation = () => {
-  return <nav>Navigation</nav>
+  return (
+    <nav className='py-4'>
+      <ul className='flex flex-col gap-2 text-sm tracking-wide text-slate-300'>
+        <li>
+          <a className='group flex cursor-pointer items-center gap-3'>
+            <span className='block h-[1px] w-12 border-[1px] border-solid border-slate-500 transition-all group-hover:w-16 group-hover:border-slate-300'></span>
+            <span className='group-hover:text-slate-200'>About</span>
+          </a>
+        </li>
+        <li>
+          <a className='group flex cursor-pointer items-center gap-3'>
+            <span className='block h-[1px] w-12 border-[1px] border-solid border-slate-500 transition-all group-hover:w-16 group-hover:border-slate-300'></span>
+            <span className='group-hover:text-slate-200'>Experience</span>
+          </a>
+        </li>
+        <li>
+          <a className='group flex cursor-pointer items-center gap-3'>
+            <span className='block h-[1px] w-12 border-[1px] border-solid border-slate-500 transition-all group-hover:w-16 group-hover:border-slate-300'></span>
+            <span className='group-hover:text-slate-200'>Education</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main
       data-element='Main'
-      className='justify-centermin-w-[375px] mx-auto flex min-h-screen max-w-screen-xl flex-wrap gap-4 border-2 border-solid border-red-500 sm:px-8 lg:flex-nowrap lg:justify-between lg:px-0'
+      className='justify-centermin-w-[375px] mx-auto flex min-h-screen max-w-screen-xl flex-wrap gap-4 sm:px-8 lg:flex-nowrap lg:justify-between lg:px-0'
     >
       <Header data-element='Header' />
       <Aside data-element='Aside' />
